de1315b7d06656519eb3faeecfc915a3f9dd20fe
PostmanPat92 <137856387+PostmanPat92@users.noreply.github.com>
2023-06-27 23:28:04 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+Interface météo à destination des usagers du réseau de transport en commun de plusieurs villes de taille moyenne en France.
+Les écrans seront intégrés aux écrans d’information dans les stations et dans les transports.
+Les écrans doivent être programmés avec les technologies web, embarquées dans la webview du système des écrans de la compagnie de transports en commun de la ville.
+
+L’interface doit inclure un fichier de configuration (JSON) dans lequel l’information de la ville concernée est entrée par l’entreprise de transport, et utilisée par le code pour récupérer les bonnes données météo.
+
+ mise à jour des données météo une fois par heure
---
+++ meteo.js
@@ -0,0 +1,32 @@
+const reponse = await fetch('config.json');
+const liste = await reponse.json();
+
+// Création des balises 
+const ville = liste[6];
+const nomElement = document.createElement("h2");
+nomElement.innerText = ville.nom;
+
+//Rattachement des balises au DOM
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(nomElement);
+// Requête à l'API OpenWeatherMap
+const apiKey = '094fc5ccae0538707f9f9f782690d695';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q= ${ville.nom} &lang=fr&appid=${apiKey}&units=metric`;
+
+fetch(apiUrl).then((response) => 
+response.json().then(data => {
+console.log(data);
+
+// Données météo
+document.querySelector('#weather').innerHTML = data.weather[0].description;// Description du temps
+document.querySelector('#temps').innerHTML = data.main.temp + '°C';// Température
+document.querySelector('#humidity').innerHTML = data.main.humidity + "%";// Humidité
+document.querySelector('#wind').innerHTML = data.wind.speed + 'km/h';// Vitesse du vent
+document.querySelector('#ressenti').innerHTML = data.main.feels_like  + '°C ressentis';// Temperature ressentie
+// Obtention de l'icone decrivant le temps en fonction de la description du temps
+let icon = data.weather[0].icon
+const img = document.querySelector('#weatherIcon');
+img.setAttribute('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
+
+  })
+);
